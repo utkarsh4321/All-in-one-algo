aff5880db73d14f9bb4e61c8ed609d977b26098a
utkarsh srivastava <30936607+utkarsh4321@users.noreply.github.com>
2024-04-14 02:35:31 +0000

    pattern-3

---
+++ patterns/pattern-3/pattern3.js
@@ -0,0 +1,10 @@
+process.stdin.on('data',(userInput)=>{
+    n = parseInt(userInput)
+    for(let i = 1;i<=n;i++){
+        for(let j = 1;j<=i;j++){
+            process.stdout.write(`${j} `);
+        }
+        process.stdout.write("\n");
+    }
+    process.exit();
+    })
